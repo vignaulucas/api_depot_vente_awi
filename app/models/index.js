4791009807481c3f54a0d3f5f318f13e5a34a72d
Lucas Vignau <lucas.vignau@etu.umontpellier.fr>
2024-10-15 15:01:34 +0200

    first commit

---
+++ app/models/index.js
@@ -0,0 +1,64 @@
+const Sequelize = require('sequelize');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Paramètres de connexion pour l'environnement local
+const localConfig = {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  options: {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    pool: {
+      max: parseInt(process.env.DB_POOL_MAX, 10),
+      min: parseInt(process.env.DB_POOL_MIN, 10),
+      acquire: parseInt(process.env.DB_ACQUIRE, 10),
+      idle: parseInt(process.env.DB_IDLE, 10),
+    },
+  },
+};
+
+// Paramètres de connexion pour l'environnement Railway (distribué)
+const railwayConfig = {
+  database: process.env.MYSQLDATABASE,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  options: {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+};
+
+// Sélectionnez la configuration appropriée en fonction de l'environnement
+const dbConfig = isProduction ? railwayConfig : localConfig;
+
+// Initialisez Sequelize avec la configuration sélectionnée
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig.options,{
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+
+  pool: {
+    max: parseInt(process.env.DB_POOL_MAX, 20),
+    min: parseInt(process.env.DB_POOL_MIN, 20),
+    acquire: parseInt(process.env.DB_ACQUIRE, 20),
+    idle: parseInt(process.env.DB_IDLE, 20),
+  },
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+
+module.exports = {
+  db,
+};
---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const { db } = require('./app/models');
+const cors = require('cors');
+const app = express();
+
+const init = require('./init');
+
+const allowedOrigins = [
+  'http://localhost:3001',
+];
+
+app.use(cors({
+  origin: allowedOrigins
+}));
+app.use(express.json({ limit: '10mb' }));
+
+db.sequelize
+  .sync()
+  .then(async () => {
+    console.log('Synced db.');
+  })
+  .catch(err => {
+    console.log('Failed to sync db: ' + err.message);
+  });
+
+app.get('/', function (req, res) {
+  res.send('Server up');
+});
+
+const port = process.env.PORT || 8082;
+app.listen(port, () => {
+  console.log(`Serveur is running on port ${port}`);
+});
